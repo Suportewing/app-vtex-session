d55dfec57ac6b267ccd23da3104ba01c0b72fabd
decobot <capy@deco.cx>
2024-10-16 15:14:30 -0300

    ajuste sessionInit

--- actions/sessionInit.ts
+++ actions/sessionInit.ts
@@ -19,9 +19,9 @@ const CepSessionInit = async (
   const vtex_session = cookies["vtex_is_session"];
   const vtex_segment = cookies["vtex_segment"];
 
-  //console.log(cookies, vtex_session, vtex_segment);
+  console.log(cookies, vtex_session, vtex_segment);
 
-  const response = await ctx.getSession[
+  const responseGet = await ctx.getSession[
     "GET /api/sessions?items=public.postalCode"
   ](
     {},
@@ -33,20 +33,22 @@ const CepSessionInit = async (
     }
   );
 
-  // const response = await ctx.session["POST /api/sessions"](
-  //   {},
-  //   {
-  //     body: data,
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //       Cookie: `vtex_session=${vtex_session}; vtex_segment=${vtex_segment}`,
-  //     },
-  //   }
-  // );
+  const response = await ctx.session["POST /api/sessions"](
+    {},
+    {
+      body: data,
+      headers: {
+        "Content-Type": "application/json",
+        Cookie: `vtex_session=${vtex_session}; vtex_segment=${vtex_segment}`,
+      },
+    }
+  );
+
+  const resultGet = await responseGet.json();
 
   const result = await response.json();
 
-  console.log("Resultado:", result);
+  console.log("Resultado:", resultGet);
 
   return result;
 };
