14bb0ec445d5b6fa65229d1a1508810fc15e2e46
Suportewing <109824101+Suportewing@users.noreply.github.com>
2024-11-12 08:45:37 -0300

    Update sessionInit.ts

--- actions/sessionInit.ts
+++ actions/sessionInit.ts
@@ -5,25 +5,39 @@ interface Props {
   data: Cep;
 }
 
-/**
- * @title This name will appear on the admin
- */
-const CepSessionInit = async (props: Props, _req: Request, ctx: AppContext): Promise<Session | null> => {
+const CepSessionInit = async (
+  props: Props,
+  _req: Request,
+  ctx: AppContext
+): Promise<Session | null> => {
   const { data } = props;
 
-  const responsePost = await ctx.session["POST /api/sessions"](
-    {},
-    {
-      body: data,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  );
+  const responseViaCep = await ctx.viaCep["GET /ws/:cep/json"]({
+    cep: data.public.postalCode.value,
+  });
 
-  const resultPost = await responsePost.json();
+  const resultViaCep = await responseViaCep.json();
 
-  return resultPost;
+  if (
+    (resultViaCep && resultViaCep.localidade === "Cascavel") ||
+    resultViaCep.localidade === "Curitiba"
+  ) {
+    const responsePost = await ctx.session["POST /api/sessions"](
+      {},
+      {
+        body: data,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const resultPost = await responsePost.json();
+
+    return resultPost;
+  }
+
+  return null;
 };
 
 export default CepSessionInit;
